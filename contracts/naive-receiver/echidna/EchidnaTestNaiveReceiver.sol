c3f5dc8176cd2ef6fcf9127e2588c1bf7faad319
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-07 18:34:40 +0100

    test: create test setup

--- contracts/naive-receiver/echidna/EchidnaTestNaiveReceiver.sol
+++ contracts/naive-receiver/echidna/EchidnaTestNaiveReceiver.sol
@@ -4,7 +4,21 @@ pragma solidity ^0.8.0;
 import "./Setup.sol";
 
 contract EchidnaTestNaiveReceiver is Setup {
-    function testAlwaysFail() public pure {
-        assert(false);
+    function balanceOfTheReceiverShouldNotDecrease(uint256 flashLoanAmount)
+        public
+    {
+        assert(true);
+        // // pre-codnitions
+        // uint256 balanceBefore = address(flashLoanReceiver).balance;
+        // // actions
+        // naiveReceiverLenderPool.flashLoan(
+        //     payable(address(flashLoanReceiver)),
+        //     flashLoanAmount
+        // );
+        // // post-conditions
+        // uint256 balanceAfter = address(flashLoanReceiver).balance;
+        // assert(balanceAfter >= balanceBefore);
     }
+
+    receive() external payable {}
 }
--- contracts/naive-receiver/echidna/Setup.sol
+++ contracts/naive-receiver/echidna/Setup.sol
@@ -3,16 +3,22 @@ pragma solidity ^0.8.0;
 
 import "../FlashLoanReceiver.sol";
 import "../NaiveReceiverLenderPool.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract Setup {
+    using Address for address payable;
     NaiveReceiverLenderPool naiveReceiverLenderPool;
     FlashLoanReceiver flashLoanReceiver;
 
-    constructor() public {
+    constructor() payable {
+        // 1 - deployment
         naiveReceiverLenderPool = new NaiveReceiverLenderPool();
         flashLoanReceiver = new FlashLoanReceiver(
             payable(address(naiveReceiverLenderPool))
         );
+        // 2 - seeding with ether
+        payable(address(naiveReceiverLenderPool)).sendValue(1000e18);
+        payable(address(flashLoanReceiver)).sendValue(10e18);
     }
 
     function _between(
